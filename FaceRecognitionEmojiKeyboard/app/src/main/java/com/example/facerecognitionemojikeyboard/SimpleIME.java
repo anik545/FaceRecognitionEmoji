dc5b8ef65744a956d62a259d8804cf0d5c1fea8a
ionescuanelise <ionescu.anelise@gmail.com>
2019-01-19 18:31:36 +0000

    keyboard changed

--- FaceRecognitionEmojiKeyboard/app/src/main/java/com/example/facerecognitionemojikeyboard/SimpleIME.java
+++ FaceRecognitionEmojiKeyboard/app/src/main/java/com/example/facerecognitionemojikeyboard/SimpleIME.java
@@ -4,6 +4,7 @@ import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.media.AudioManager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
@@ -15,6 +16,7 @@ public class SimpleIME extends InputMethodService
     private Keyboard keyboard;
      
     private boolean caps = false;
+    private boolean numbers = false;
  
     @Override
     public void onPress(int primaryCode) {
@@ -46,10 +48,27 @@ public class SimpleIME extends InputMethodService
 
     @Override
     public View onCreateInputView() {
-        kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard, null);
-        keyboard = new Keyboard(this, R.xml.qwerty);
+        View view = setView(false);
+        return view;
+    }
+
+    private View setView(boolean iSet) {
+        if( numbers ) {
+            kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
+            keyboard = new Keyboard(this, R.xml.numbers);
+        }
+        else {
+            kv = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard, null);
+            keyboard = new Keyboard(this, R.xml.qwerty);
+        }
+        Log.d("Set view", "After if");
         kv.setKeyboard(keyboard);
         kv.setOnKeyboardActionListener(this);
+        Log.d("Set view", "After kv");
+        if (iSet) {
+            setInputView(kv);
+        }
+        Log.d("Set view", "After set");
         return kv;
     }
 
@@ -88,6 +107,11 @@ public class SimpleIME extends InputMethodService
                 break;
             case -10:
                 //TODO: when camera is pressed
+                break;
+            case -6:
+                numbers = !numbers;
+                setView(true);
+                break;
             default:
                 char code = (char)primaryCode;
                 if(Character.isLetter(code) && caps){
